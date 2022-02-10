9d8a02a7ae9aab1caa2405b9f70cdbdeb17709f3
Vivioza5 <51415071+Vivioza5@users.noreply.github.com>
2022-02-10 21:42:41 +0200

    Add files via upload

---
+++ src/test/java/my/company/SimpleTest.java
@@ -0,0 +1,48 @@
+package my.company;
+
+import my.company.Pages.Chrome;
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+/**
+ * @author baev (Dmitry Baev)
+ */
+public class SimpleTest {
+WebDriver driver;
+
+    @BeforeTest
+    public void setUp() throws IOException {
+        driver = Chrome.getInstanceOfChrome();
+    }
+
+
+    @Test
+    public void simpleTestOne() {
+String login_url = "https://opensource-demo.orangehrmlive.com/";
+        driver.get(login_url);
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+        driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+        System.out.println(driver.getTitle());
+    }
+    @Test
+    public void simpleTestTwo() {
+        step("step 1");
+        step("step 2");
+    }
+        @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
+
